c2d57f0ec1be1d05d3b57998dda190e2b2f0e78f
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-10-22 23:18:33 +0300

    Added input cleaning

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const API_KEY = "AIzaSyBtpCmb4WHmKy3I10fzVprfjfW172m8ZnQ";
 const searchForm = document.getElementById("form");
 const containerVideo = document.getElementById("container-videos");
+const input = document.getElementById("input");
+const spinner = `<div class="col-12 d-flex justify-content-center "><div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div></div>`;
 
 const getYoutubeVideos = (query) =>
   fetch(
@@ -13,6 +17,8 @@ searchForm.addEventListener("submit", (e) => {
   const videoName = formData.get("video-name");
   const response = getYoutubeVideos(videoName);
   const videosIDs = [];
+  containerVideo.innerHTML = spinner;
+
   response
     .then((result) => result.json())
     .then((videos) => {
@@ -20,14 +26,21 @@ searchForm.addEventListener("submit", (e) => {
       videos.items.forEach((video) => videosIDs.push(video.id.videoId));
     })
     .then(() => {
-      videosIDs.forEach((videoID) => {
-        const card = document.createElement("div");
-        card.innerHTML = `<div class="col">
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/${videoID}" 
+      containerVideo.innerHTML = "";
+      if (!videosIDs.length) {
+        containerVideo.innerHTML = `<h5 class="col-12 text-center">Videos not found</h5>`;
+        input.value = "";
+      } else {
+        videosIDs.forEach((videoID) => {
+          const card = document.createElement("div");
+          card.innerHTML = `<div class="col">
+        <iframe width="356" height="200" src="https://www.youtube.com/embed/${videoID}" 
         frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
         ></iframe>
         </div>`;
-        containerVideo.appendChild(card);
-      });
+          containerVideo.appendChild(card);
+          input.value = "";
+        });
+      }
     });
 });
